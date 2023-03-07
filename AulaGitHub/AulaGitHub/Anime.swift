811550e8c2501fd572aacc63a22b9f7005f0d348
Gilmar Amaro <gilmar.amaro13@gmail.com>
2023-03-07 17:51:52 -0300

    adicionei NSObect, e criei as Variaveis com as informacoes

---
+++ AulaGitHub/AulaGitHub/Anime.swift
@@ -0,0 +1,19 @@
+//
+//  Anime.swift
+//  AulaGitHub
+//
+//  Created by Gilmar Amaro on 07/03/23.
+//
+
+import UIKit
+
+class Anime: NSObject {
+    var nameAnime: String = ""
+    var descriptionAnime: String = ""
+    var nameCharacter: String = ""
+    var descriptionCharacter: String = ""
+    var nameCharacter2: String = ""
+    var descriptionCharacter2: String = ""
+    var nameCharacter3: String = ""
+    var descriptionCharacter3: String = ""
+}
